a16fb9e4dd896a47e06670d69910abaeacd90384
kelemul <kelemu3509@gmail.com>
2022-08-26 02:45:31 -0700

    linked list done

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,38 @@
+/*
+ * File: 13-insert_number.c
+ * Auth: Kelemu L.
+ */
+
+#include "lists.h"
+
+/**
+ * insert_node - add new node value.
+ * @head: A pointer to the start of linked list.
+ * @number: The new node value.
+ *
+ * Return: NULL or the new value.
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *node = *head, *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+
+	if (node == NULL || node->n >= number)
+	{
+		new->next = node;
+		*head = new;
+		return (new);
+	}
+
+	while (node && node->next && node->next->n < number)
+		node = node->next;
+
+	new->next = node->next;
+	node->next = new;
+
+	return (new);
+}
