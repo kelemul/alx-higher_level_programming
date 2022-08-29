a754d490bf534eb74472416af3bcd5f470891328
kelemul <kelemu3509@gmail.com>
2022-08-29 04:59:41 -0700

    done max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 9-max_integer.py
+# kelemu l.
+
+
+def max_integer(my_list=[]):
+    """get biggest integer."""
+    if len(my_list) == 0:
+        return (None)
+
+    big = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > big:
+            big = my_list[i]
+
+    return (big)
