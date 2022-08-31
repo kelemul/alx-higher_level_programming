cffcc29f87705813e8c6ded01334216477f86329
kelemul <kelemu3509@gmail.com>
2022-08-31 00:28:50 -0700

    done second part

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 3-common_elements.py
+# kelemu L.
+
+
+def common_elements(set_1, set_2):
+    """get common elements in two list."""
+    return (set_1 & set_2)
---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 4-only_diff_elements.py
+# kelemu L.
+
+
+def only_diff_elements(set_1, set_2):
+    """return one set elements only."""
+    return (set_1 ^ set_2)
