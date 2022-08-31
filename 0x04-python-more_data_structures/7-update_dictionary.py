cd8b831a8813d9fee2e95bb492e2cb4eba648806
kelemul <kelemu3509@gmail.com>
2022-08-31 00:38:32 -0700

    done deleting

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 7-update_dictionary.py
+# kelemmu L.
+
+
+def update_dictionary(a_dictionary, key, value):
+    """add or replace values in a dict."""
+    a_dictionary[key] = value
+    return (a_dictionary)
